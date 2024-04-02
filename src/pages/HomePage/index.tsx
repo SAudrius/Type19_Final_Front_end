2377135493e5457e67174b57a838b7460154ed67
SAudrius <audrius@sabalys.com>
2024-04-02 12:54:39 +0300

    test(cn): test cn function

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,7 +1,15 @@
+import { cn } from '../../lib/utils';
+
 export const HomePage = () => {
   return (
     <div>
-      <h2 className='text-4xl mt-4 text-bold text-center'>HomePage</h2>
+      <h2
+        className={cn('text-4xl mt-4 text-bold text-center', {
+          'text-blue-900': true,
+        })}
+      >
+        HomePage
+      </h2>
     </div>
   );
 };
