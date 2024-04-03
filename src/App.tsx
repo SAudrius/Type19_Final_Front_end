682569221e84f0a660e605f0f34dff6ba5917168
SAudrius <audrius@sabalys.com>
2024-04-03 17:02:13 +0300

    feat(head): created header without logic logged in

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ export const App = () => {
         <Route path='/ad/:id' element={<AdPage />}></Route>
         <Route path='/towns' element={<TownsPage />}></Route>
         <Route path='/town/:townId' element={<TownPage />}></Route>
-        <Route path='/user/:userId' element={<UserPage />}></Route>
+        <Route path='/user' element={<UserPage />}></Route>
         <Route path='/login' element={<LoginPage />}></Route>
         <Route path='/register' element={<RegisterPage />}></Route>
         <Route path='/*' element={<NotFoundPage />}></Route>
