30ed461e12adafeea0467cba5fa725c5aa9dd9e4
SAudrius <audrius@sabalys.com>
2024-04-15 01:02:38 +0300

    feat(global): loading and error global components

--- src/components/ui/GlobalLoading/index.tsx
+++ src/components/ui/GlobalLoading/index.tsx
@@ -1,9 +1,28 @@
 import ClipLoader from "react-spinners/ClipLoader";
 
-export const GlobalLoading = () => {
+import { cn } from "@/lib/utils";
+
+interface GlobalLoadingProps {
+  className?: string;
+  size?: "huge" | "default" | "small";
+}
+export const GlobalLoading = ({
+  className,
+  size = "default",
+}: GlobalLoadingProps) => {
+  let customSize;
+  if (size === "huge") {
+    customSize = 100;
+  }
+  if (size === "default") {
+    customSize = 50;
+  }
+  if (size === "small") {
+    customSize = 30;
+  }
   return (
-    <div className="flex justify-center rounded-md bg-green-50 px-4 py-2">
-      <ClipLoader color="#F39316" />
+    <div className={cn("flex justify-center rounded-md px-4 py-2", className)}>
+      <ClipLoader size={customSize} color="#F39316" />
     </div>
   );
 };
