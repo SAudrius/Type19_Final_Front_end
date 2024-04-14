18573d590352192e32afda570992a5fc9009a6c1
SAudrius <audrius@sabalys.com>
2024-04-15 01:04:30 +0300

    feat(api): api auth instance for auth routes

--- src/utils/api/requests/classifiedAds/index.ts
+++ src/utils/api/requests/classifiedAds/index.ts
@@ -1,4 +1,7 @@
-import { api } from "@api/instance";
+import { api, authApi } from "@api/instance";
 
 export const getClassifiedAds = async () =>
   api.get<ClassifiedAd[]>("/classified-ads");
+
+export const deleteClassifiedAds = async (token: string, id: number) =>
+  authApi(token).delete<ClassifiedAd[]>(`/classified-ads/${id}`);
