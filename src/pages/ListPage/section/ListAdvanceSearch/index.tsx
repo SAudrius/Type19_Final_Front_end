a0f4dfd4d2bd9b046ab1b6b88fcdbaab599db5dd
SAudrius <audrius@sabalys.com>
2024-04-15 11:17:48 +0300

    fix: error state

--- src/pages/ListPage/section/ListAdvanceSearch/index.tsx
+++ src/pages/ListPage/section/ListAdvanceSearch/index.tsx
@@ -62,7 +62,7 @@ export const ListAdvanceSearch = ({
       dispatch(setSearchTownName("Select Town"));
       dispatch(setSearchValue(""));
     } catch {
-      // setCardError();
+      setError(true);
     }
 
     setCardsLoading(false);
